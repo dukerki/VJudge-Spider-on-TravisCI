e17d9c3c94ba94d7d1f14b38d55a0a8e84264d35
Zhang1933 <193319076@qq.com>
2022-01-16 10:20:25 +0800

    测试上传失败检查

--- CheckHealthy.sh
+++ CheckHealthy.sh
@@ -12,5 +12,6 @@ if cmp -s "$file1" "$file2"; then
     echo "成功,远程仓库上out.csv文件已更改" 
 else 
     echo "ERROR:远程仓库上文件未更改,上传仓库失败。可能github token已失效，需要在设置中同名替换新的有效token。"
-    travis_terminate 1
+    exit 1
 fi
+exit 0
