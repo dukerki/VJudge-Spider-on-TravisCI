2772de76493d4f3328e8b047bf178bc80e65687d
Zhang1933 <193319076@qq.com>
2022-01-16 09:14:01 +0800

    修复检查脚本bug

--- CheckHealthy.sh
+++ CheckHealthy.sh
@@ -9,7 +9,7 @@ wget "$out"
 file1="out.csv"
 file2="../out.csv"
 
-if cmp -s "$file1" "$file2": then
+if cmp -s "$file1" "$file2"; then
     echo "成功,远程仓库上out.csv文件已更改" 
 else 
     echo "远程仓库上文件未更改,上传仓库失败。可能github token已失效，需要在设置中同名替换新token"
