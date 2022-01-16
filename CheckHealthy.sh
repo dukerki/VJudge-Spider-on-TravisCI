88fe4fb8440904ff7b7e63522faad42c5893fd69
Zhang1933 <193319076@qq.com>
2022-01-16 09:10:44 +0800

    添加上传仓库成功测试功能

---
+++ CheckHealthy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+$out="https://raw.githubusercontent.com/${USER}/${REPO}/blob/${TRAVIS_BRANCH}/out.csv"
+
+mv out.csv ..
+
+wget "$out"
+
+file1="out.csv"
+file2="../out.csv"
+
+if cmp -s "$file1" "$file2": then
+    echo "成功,远程仓库上out.csv文件已更改" 
+else 
+    echo "远程仓库上文件未更改,上传仓库失败。可能github token已失效，需要在设置中同名替换新token"
+    exit 1;
+fi
