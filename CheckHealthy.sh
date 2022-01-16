1e0ca7b0dbe2a7dfff1f1e1b8826f88a5dc80cca
Zhang1933 <193319076@qq.com>
2022-01-16 10:02:03 +0800

    test healthy check

--- CheckHealthy.sh
+++ CheckHealthy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-out="https://raw.githubusercontent.com/${USER}/${REPO}/blob/${TRAVIS_BRANCH}/out.csv"
+out="https://raw.githubusercontent.com/${USER}/${REPO}/${TRAVIS_BRANCH}/out.csv"
 
 wget -O ../${FILES}  "$out" 
 
@@ -10,6 +10,6 @@ file2="../out.csv"
 if cmp -s "$file1" "$file2"; then
     echo "成功,远程仓库上out.csv文件已更改" 
 else 
-    echo "ERROR:远程仓库上文件未更改,上传仓库失败。可能github token已失效，需要在设置中同名替换新的有效token"
-    exit 1;
+    echo "ERROR:远程仓库上文件未更改,上传仓库失败。可能github token已失效，需要在设置中同名替换新的有效token。"
+    travis_terminate 1
 fi
