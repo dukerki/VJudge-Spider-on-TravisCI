62d0460f94cb0fa63890688c7e5c3143dc3308e8
杜可 <161901040+dukerki@users.noreply.github.com>
2025-01-11 19:42:38 +0800

    Update get_url.py

--- get_url.py
+++ get_url.py
@@ -59,7 +59,7 @@ def select_elements():
     '''选出后续需要使用的元素'''
 
     search_box = driver.find_element("xpath",r'/html/body/div[1]/div/div[2]/div/table/thead/tr/th[3]/input')
-    print(driver)
+    
     search_box.send_keys('CUC\n')
 
     contents_sort = driver.find_element(
